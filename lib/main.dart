e1f1abcb0f4e664243be776e33646099ffcca79a
toyohide <hide.toyoda@gmail.com>
2023-10-31 09:26:17 +0900

    commit 20231031

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
+import 'dart:convert';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 import 'firebase_options.dart';
 
 //////////////////////////////////////////////
-Future _firebaseBackgroundMessage(RemoteMessage message) async {
+Future<void> _firebaseBackgroundMessage(RemoteMessage message) async {
   if (message.notification != null) {
-    print('get some noti');
+    debugPrint('get some noti');
   }
 }
 
@@ -18,13 +21,27 @@ void main() async {
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  PushNotifications.init();
+  await PushNotifications.init();
+  await PushNotifications.localNotiInit();
 
   FirebaseMessaging.onBackgroundMessage(_firebaseBackgroundMessage);
 
   FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
     if (message.notification != null) {
       navigatorKey.currentState!.pushNamed('/message', arguments: message);
+    }
+  });
+
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    final payloadData = jsonEncode(message.data);
+
+    print('get foreground noti');
+    if (message.notification != null) {
+      PushNotifications.showSimpleNotification(
+        title: message.notification!.title!,
+        body: message.notification!.body!,
+        payload: payloadData,
+      );
     }
   });
 
@@ -58,7 +75,7 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: SafeArea(
         child: Column(
           children: [
@@ -75,20 +92,62 @@ class HomePage extends StatelessWidget {
 class PushNotifications {
   static final _firebaseMessaging = FirebaseMessaging.instance;
 
+  static final FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
   ///
-  static Future init() async {
+  static Future<void> init() async {
     await _firebaseMessaging.requestPermission(
-      alert: true,
       announcement: true,
-      badge: true,
-      carPlay: false,
-      criticalAlert: false,
-      provisional: false,
-      sound: true,
     );
 
     final token = await _firebaseMessaging.getToken();
-    print('token: ${token}');
+    print('token: $token');
+  }
+
+  ///
+  static Future<void> localNotiInit() async {
+    // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
+    const initializationSettingsAndroid = AndroidInitializationSettings('@mipmap/ic_launcher');
+
+    final initializationSettingsDarwin = DarwinInitializationSettings(
+      onDidReceiveLocalNotification: (id, title, body, payload) {},
+    );
+
+    const initializationSettingsLinux = LinuxInitializationSettings(defaultActionName: 'Open notification');
+
+    final initializationSettings = InitializationSettings(
+        android: initializationSettingsAndroid, iOS: initializationSettingsDarwin, linux: initializationSettingsLinux);
+
+    await _flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+      onDidReceiveNotificationResponse: onNotificationTap,
+      onDidReceiveBackgroundNotificationResponse: onNotificationTap,
+    );
+  }
+
+  ///
+  static void onNotificationTap(NotificationResponse notificationResponse) {
+    navigatorKey.currentState!.pushNamed('/message', arguments: notificationResponse);
+  }
+
+  ///
+  static Future<void> showSimpleNotification({
+    required String title,
+    required String body,
+    required String payload,
+  }) async {
+    const androidNotificationDetails = AndroidNotificationDetails(
+      'your channel id',
+      'your channel name',
+      channelDescription: 'your channel description',
+      importance: Importance.max,
+      priority: Priority.high,
+      ticker: 'ticker',
+    );
+
+    const notificationDetails = NotificationDetails(android: androidNotificationDetails);
+
+    await _flutterLocalNotificationsPlugin.show(0, title, body, notificationDetails, payload: payload);
   }
 }
 
@@ -102,13 +161,23 @@ class Message extends StatefulWidget {
 }
 
 class _MessageState extends State<Message> {
+  Map<dynamic, dynamic> payload = {};
+
+  ///
   @override
   Widget build(BuildContext context) {
-    final notiData = ModalRoute.of(context)!.settings.arguments as RemoteMessage;
+//    final notiData = ModalRoute.of(context)!.settings.arguments as RemoteMessage;
+    final notiData = ModalRoute.of(context)!.settings.arguments;
+    if (notiData is RemoteMessage) {
+      payload = notiData.data;
+    }
+    if (notiData is NotificationResponse) {
+      payload = jsonDecode(notiData.payload!);
+    }
 
     return Scaffold(
-      appBar: AppBar(title: Text('Noti Tapped')),
-      body: SafeArea(child: Text(notiData.data.toString())),
+      appBar: AppBar(title: const Text('Noti Tapped')),
+      body: SafeArea(child: Text(payload.toString())),
     );
   }
 }
